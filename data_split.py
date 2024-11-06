bc4a8e49fdf753ede781d433ad734b22ca54465d
Nikitha2901 <nikithamurthy2910@gmail.com>
2024-11-06 19:00:56 +0100

    script to split into different datasets for annotation

---
+++ data_split.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import os
+import numpy as np
+
+#loading the data 
+data_main = pd.read_csv("/home/nikitha/swerik/data.csv")
+
+#splitting the data into 3 parts
+split_value = np.array_split(data_main,3)
+
+output_data = "/home/nikitha/swerik/data_partitioned"
+os.makedirs(output_data,exist_ok=True)
+for i,split in enumerate(split_value):
+    split.to_csv(f"{output_data}/part_{i+1}.csv", index=False)
+
+#to verify length of files
+for i, split in enumerate(split_value):
+    print(f"Rows in part {i+1}: {len(split)}")
+
+
