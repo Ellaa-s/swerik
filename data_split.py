fe72e0d6e311a4043f6e27138f4bb53ffec6b638
Ellaa-s <ella.schmidtobreick.4283@student.uu.se>
2024-11-10 01:31:10 +0100

    new split

--- data_split.py
+++ data_split.py
@@ -3,12 +3,12 @@ import os
 import numpy as np
 
 #loading the data 
-data_main = pd.read_csv("/home/nikitha/swerik/output.csv")
+data_main = pd.read_csv("./data.csv")
 
 #splitting the data into 3 parts
 split_value = np.array_split(data_main,3)
 
-output_data = "/home/nikitha/swerik/data_partitioned"
+output_data = "./data_partitioned"
 os.makedirs(output_data,exist_ok=True)
 for i,split in enumerate(split_value):
     split.to_csv(f"{output_data}/part_{i+1}.csv", index=False)
